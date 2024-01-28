2dae5841a717c637f65c63f1e9f6d115aa52fd21
CHIBUIKE UBAH <83553647+ubahchisam@users.noreply.github.com>
2024-01-28 15:34:06 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,51 @@
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+searchBox.addEventListener("click", ()=>{
+  navbar.classList.toggle("showInput");
+  if(navbar.classList.contains("showInput")){
+    searchBox.classList.replace("bx-search" ,"bx-x");
+  }else {
+    searchBox.classList.replace("bx-x" ,"bx-search");
+  }
+});
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+  navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+  navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let assertsArrow = document.querySelector(".asserts-arrow");
+assertsArrow.onclick = function() {
+  navLinks.classList.toggle("show1");
+}
+//counter
+const displayValues = document.querySelectorAll(".num");
+
+let intervals = 5000;
+
+displayValues.forEach((displayValue) => {
+    let startValue = 0;
+
+    let endValue = parseInt(displayValue.getAttribute("data-val"));
+
+    let duration = Math.floor(intervals / endValue)
+
+    let counter = setInterval(() => {
+        startValue ++;
+        displayValue.innerHTML = startValue;
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration);
+})
